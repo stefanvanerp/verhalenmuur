0f76ea73918080cc7952fc53d603704194eb3b90
stefanvanerp <stefan@triplepentertainment.nl>
2026-04-28 15:35:03 +0200

    Update route.js

--- app/api/instagram/webhook/route.js
+++ app/api/instagram/webhook/route.js
@@ -1,18 +1,3 @@
-export async function GET(req) {
-  const { searchParams } = new URL(req.url);
-
-  const mode = searchParams.get('hub.mode');
-  const token = searchParams.get('hub.verify_token');
-  const challenge = searchParams.get('hub.challenge');
-
-  const VERIFY_TOKEN = 'premiere2026';
-
-  if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-    return new Response(challenge, { status: 200 });
-  }
-
-  return new Response('Forbidden', { status: 403 });
-}
 export async function GET(request) {
   const { searchParams } = new URL(request.url);
 
